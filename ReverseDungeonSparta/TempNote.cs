13842e673f42748538f46e20074cc502b150f351
thdwpdn26 <songjeu26@naver.com>
2025-02-08 03:34:35 +0900

    [Docs, Style] 메서드 주석 추가 및 선언 변경
    TurnPreview의 List<Character>를 Character로 변경.
    
    주요 메서드 주석 추가.

--- ReverseDungeonSparta/TempNote.cs
+++ ReverseDungeonSparta/TempNote.cs
@@ -9,25 +9,29 @@ using ReverseDungeonSparta;
 public class TurnManager
 {
     public List<Character> Characters { get; private set; }
-    public List<Character> TurnPreview { get; private set; }
+    public Character SeclectCharacter { get; private set; }
     public List<(Character, double)> turnQueue { get; private set; }
 
+
+    //턴 매니저 생성자
     public TurnManager(List<Character> characters)
     {
         Characters = characters;
-        TurnPreview = new List<Character>();
+        SeclectCharacter = new Character();
         turnQueue = new List<(Character, double)>();
         turnQueue = Characters
-    .Select(c => (c, 100.0 / c.Speed))
+    .Select(c => (c, 100.0d / c.Speed))
     .ToList();
     }
 
 
+    //캐릭터 리스트를 스피드의 순서대로 정렬한 후 맨 앞의 캐릭터를 가져온 후 SeclectCharacter에 저장
+    //이후 해당 캐릭터를 리스트에서 삭제한 후 속도를 더한 값으로 재정의하여 다시 리스트의 속도 순서에 맞게 추가함.
     public void CalculateTurnPreview(int previewTurns = 5)
     {
-        TurnPreview.Clear();
+        SeclectCharacter = new Character();
         var nextCharacter = turnQueue.OrderBy(t => t.Item2).First();
-        TurnPreview.Add(nextCharacter.Item1);
+        SeclectCharacter = nextCharacter.Item1;
         turnQueue = turnQueue
             .Where(t => t.Item1 != nextCharacter.Item1)
             .Append((nextCharacter.Item1, nextCharacter.Item2 + 100.0 / nextCharacter.Item1.Speed))
