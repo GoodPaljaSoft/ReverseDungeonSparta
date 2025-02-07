89111375ed018048f4f26f24b90bc4abd541e284
thdwpdn26 <songjeu26@naver.com>
2025-02-07 23:51:58 +0900

    [Fix] "턴이 반복적으로 실행되는 버그 해결"
    가장 높은 속도를 가진 객체가 계속 턴이 반복되는 버그를 해결. turnQueue List를 클래스 필드로 옮김.

--- ReverseDungeonSparta/TempNote.cs
+++ ReverseDungeonSparta/TempNote.cs
@@ -1,70 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
+using System.Threading;
 using ReverseDungeonSparta;
 
 public class TurnManager
 {
     public List<Character> Characters { get; private set; }
     public List<Character> TurnPreview { get; private set; }
+    public List<(Character, double)> turnQueue { get; private set; }
 
-    public TurnManager()
+    public TurnManager(List<Character> characters)
     {
-        Characters = new List<Character>();
+        Characters = characters;
         TurnPreview = new List<Character>();
+        turnQueue = new List<(Character, double)>();
+        turnQueue = Characters
+    .Select(c => (c, 100.0 / c.Speed))
+    .ToList();
     }
 
 
     public void CalculateTurnPreview(int previewTurns = 5)
     {
-        var turnQueue = Characters
-            .Select(c => (character: c, nextTurn: 100.0 / c.Speed))
-            .ToList();
-
-        //플레이어 스피드 10 -> 먼저 실행 10
-        //나머지 몬스터   5               20
-
         TurnPreview.Clear();
-
-        for (int i = 0; i < previewTurns; i++)
-        {
-            var nextCharacter = turnQueue.OrderBy(t => t.nextTurn).First();
-            TurnPreview.Add(nextCharacter.character);
-            turnQueue = turnQueue
-                .Where(t => t.character != nextCharacter.character)
-                .Append((character: nextCharacter.character, nextTurn: nextCharacter.nextTurn + 100.0 / nextCharacter.character.Speed))
-                .OrderBy(t => t.nextTurn)
-                .ToList();
-        }
-    }
-}
-
-public class BattleManager2
-{
-    public event Action<Character> OnTurnStart; // 턴 시작 이벤트
-    private TurnManager turnManager;
-
-    public void Battle()
-    {
-        turnManager.CalculateTurnPreview(); // 5턴 프리뷰를 계산
-        var currentCharacter = turnManager.TurnPreview.First();
-
-        TurnPlay(currentCharacter);
-    }
-
-    public void TurnPlay(Character character)
-    {
-        // 턴 시작 시 이벤트 발생
-        OnTurnStart?.Invoke(character);
-
-        // 현재 턴 처리 로직
-        Console.WriteLine($"{character.Name}의 턴");
-
-        // 예시: 몬스터가 사망하면 Speed를 0으로 설정 턴에서 제외
-        if (/*몬스터라면*/)
-        {
-            Console.WriteLine($"{character.Name} 디짐");
-            character.Speed = 0;
-        }
+        var nextCharacter = turnQueue.OrderBy(t => t.Item2).First();
+        TurnPreview.Add(nextCharacter.Item1);
+        turnQueue = turnQueue
+            .Where(t => t.Item1 != nextCharacter.Item1)
+            .Append((nextCharacter.Item1, nextCharacter.Item2 + 100.0 / nextCharacter.Item1.Speed))
+            .OrderBy(t => t.Item2)
+            .ToList();
     }
 }
