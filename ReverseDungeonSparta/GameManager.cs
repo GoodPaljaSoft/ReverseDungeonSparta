e0f87b675c92c4311cb20c7f11335deec724c771
thdwpdn26 <songjeu26@naver.com>
2025-02-09 04:20:07 +0900

    [Feat] 번호 입력이 아닌 화살표 입력으로 임시 변경
    게임 매니저에서 테스트로 번호 입력이 아닌, 플레이어의 화살표 입력으로 선택지를 선택하여 Enter키를 통해 이동하는 로직 구현.
    
    이 방법을 선택하여 모두 적용할지 상의 필요함.

--- ReverseDungeonSparta/GameManager.cs
+++ ReverseDungeonSparta/GameManager.cs
@@ -10,45 +10,82 @@ namespace ReverseDungeonSparta
     class GameManager
     {
         public static GameManager Instance { get; } = new GameManager();
-       
+
         Player player = new Player();
+        BattleManager BattleManagerInstance { get; set; }
+
+        int selectedIndex = 0;
 
         public GameManager()
         {
-
+            BattleManagerInstance = new BattleManager(player);
         }
+
+        public void PlayerStatusMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("상태보기");
+            Console.WriteLine("캐릭터의 정보가 표시됩니다.");
+            Util.PrintPlayerView(player);
+        }
+
+        public void EnterPlayerStatusMenu()
+        {
+            AudioManager.PlayMoveMenuSE(0);
+            PlayerStatusMenu();
+        }
+
+        public void EnterBattleMenu()
+        {
+            AudioManager.PlayBattleBGM();
+            AudioManager.PlayMoveMenuSE(0);
+            BattleManagerInstance.StartBattle();
+        }
+
         public void GameMenu() // 시작화면 구현
         {
+            Console.CursorVisible = false;
             Console.Clear();
             Console.WriteLine("스파르타 마을에 오신 여러분 환영입니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.");
-            Console.WriteLine($"\n1. 상태 보기");
-            Console.WriteLine("2. 전투 시작");
             Console.WriteLine("");
-    
-            int result = Util.GetUserInput(1, 2);
 
-            switch (result)
+            List<(String, Action)> menuItems = new List<(string, Action)>
             {
-                case 1: // 상태보기화면 구현
-                    Console.Clear();
-                    Console.WriteLine("상태보기");
-                    Console.WriteLine("캐릭터의 정보가 표시됩니다.");
-                    Util.PrintPlayerView(player);
-                    AudioManager.PlayMoveMenuSE(0);
-                    int outMenu = Util.GetUserInput(0, 0);
-                    if(outMenu == 0)
-                    {
-                        AudioManager.PlayMoveMenuSE(0);
-                        GameMenu();
-                    }
-                    break;
-                case 2: //전투시작화면 
-                    BattleManager battleManager = new BattleManager(player);
-                    AudioManager.PlayBattleBGM();
-                    AudioManager.PlayMoveMenuSE(0);
-                    battleManager.StartBattle();
+                ("1. 상태 보기", PlayerStatusMenu),
+                ("2. 전투 시작", EnterBattleMenu)
+            };
 
+
+
+            for(int i = 0; i < menuItems.Count; i++)
+            {
+                if (i == selectedIndex) Console.WriteLine($"-> {menuItems[i].Item1}");
+                else                    Console.WriteLine($"   {menuItems[i].Item1}");
+            }
+
+
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if(selectedIndex > 0)
+                    {
+                        selectedIndex--;
+                    }
+                    GameMenu();
+                    break;
+                case ConsoleKey.DownArrow:
+                    if (selectedIndex < menuItems.Count - 1)
+                    {
+                        selectedIndex++;
+                    }
+                    GameMenu();
+                    break;
+                case ConsoleKey.Enter:
+                    menuItems[selectedIndex].Item2();
+                    break;
+                default:
                     GameMenu();
                     break;
             }
