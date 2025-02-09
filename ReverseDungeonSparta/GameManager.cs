2e376b74dcb37adbd31bae3c31866927e5760359
thdwpdn26 <songjeu26@naver.com>
2025-02-09 18:08:48 +0900

    [Feat]게임 메니저 입력 방식 변환

--- ReverseDungeonSparta/GameManager.cs
+++ ReverseDungeonSparta/GameManager.cs
@@ -19,6 +19,7 @@ namespace ReverseDungeonSparta
         public GameManager()
         {
             BattleManagerInstance = new BattleManager(player);
+            Console.CursorVisible = false;          //깜빡이는 커서를 비활성화
         }
 
         public void PlayerStatusMenu()
@@ -44,51 +45,29 @@ namespace ReverseDungeonSparta
 
         public void GameMenu() // 시작화면 구현
         {
-            Console.CursorVisible = false;
+            //고정으로 출력할 텍스트를 위쪽에 미리 그려둡니다.
             Console.Clear();
             Console.WriteLine("스파르타 마을에 오신 여러분 환영입니다.");
             Console.WriteLine("이제 전투를 시작할 수 있습니다.");
             Console.WriteLine("");
 
+
+            //선택지로 출력할 텍스트와 진입할 메소드를 menuItems의 요소로 집어 넣어줍니다.
             List<(String, Action)> menuItems = new List<(string, Action)>
             {
                 ("1. 상태 보기", PlayerStatusMenu),
                 ("2. 전투 시작", EnterBattleMenu)
+                //추가 할 경우
+                //("3. 아이템 메뉴", [아이테 메뉴에 진입하는 메소드 이름])
+                //...
             };
 
 
-
-            for(int i = 0; i < menuItems.Count; i++)
-            {
-                if (i == selectedIndex) Console.WriteLine($"-> {menuItems[i].Item1}");
-                else                    Console.WriteLine($"   {menuItems[i].Item1}");
-            }
-
-
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            switch (keyInfo.Key)
-            {
-                case ConsoleKey.UpArrow:
-                    if(selectedIndex > 0)
-                    {
-                        selectedIndex--;
-                    }
-                    GameMenu();
-                    break;
-                case ConsoleKey.DownArrow:
-                    if (selectedIndex < menuItems.Count - 1)
-                    {
-                        selectedIndex++;
-                    }
-                    GameMenu();
-                    break;
-                case ConsoleKey.Enter:
-                    menuItems[selectedIndex].Item2();
-                    break;
-                default:
-                    GameMenu();
-                    break;
-            }
+            //Util.GetUserInput은
+            //1. 만들어준 List<(String, Action)> 목록
+            //2. 해당 유틸을 실행하는 본인 메서드
+            //3. 클래스 필드에서 선언한 int 변수를 ref형태로 넣습니다.
+            Util.GetUserInput(menuItems, GameMenu, ref selectedIndex);
         }
     }
 }
