1a1ccb50b6e9edc9b9c33c9bb9394499653e1488
윤동영 <ehddud9608@naver.com>
2025-02-12 00:20:14 +0900

    Merge branch 'Dev2_YHJ_SJW' of https://github.com/GoodPaljaSoft/ReverseDungeonSparta into Dev2_YHJ_SJW

--- ReverseDungeonSparta/InventoryViewManager.cs
+++ ReverseDungeonSparta/InventoryViewManager.cs
@@ -19,15 +19,72 @@ namespace ReverseDungeonSparta
             ViewManager.PrintText("   나가기");
         }
 
-        public static void InventoryEquippedMenuTxt()
+        public static void InventoryEquippedMenuTxt(List<EquipItem> itemList, ref int selectedIndex, (int, int) cursor)
         {
             Console.Clear();
             PrintTitleTxt("소지품 확인 - 장비", 12);
             ViewManager.PrintText(0, 27, "   장비 장착");
             ViewManager.PrintText("   장비 합성");
-            ViewManager.PrintText("   나가기");
+            ViewManager.PrintText("[C]나가기");
+
+            List<(string, Action, Action)> itemScrollView = itemList
+                                                        .Select(x => (InventorySortList(x), (Action)null, (Action)null))
+                                                        .ToList();
+
+            ScrollViewTxt(itemScrollView, ref selectedIndex, cursor);
         }
 
+        public static string InventorySortList(EquipItem equipItem)
+        {
+            int[] optionArray = { equipItem.AddLuck, equipItem.AddDefence, equipItem.AddAttack, equipItem.AddIntelligence, equipItem.AddMaxHp, equipItem.AddMaxMp };
+            string[] nameArray = { "AddLuck", "AddDefence", "AddAttack", "AddIntelligence", "AddMaxHp", "AddMaxMp" };
+            StringBuilder sb = new StringBuilder();
+            bool isEquipped = equipItem.IsEquiped;
+            sb.Append(isEquipped ? " [E] ".PadRight(5) : " [-] ".PadRight(5));
+            sb.Append(equipItem.Name.PadRight(20));
+            sb.Append($"| {TranslateString(equipItem.Type.ToString())}");
+            sb.Append($"{equipItem.Information}\n");
+            for(int i = 0; i<optionArray.Length; i++)
+            {
+                if (optionArray[i] != 0)
+                {
+                    sb.Append($"|{TranslateString(nameArray[i])} +{optionArray[i]}".PadLeft(50));
+                } 
+            }
+            return sb.ToString();
+        }
+        public static string TranslateString(string enumType)
+        {
+            switch (enumType)
+            {
+                case "Armor":
+                    return "방어구";
+                case "Weapon":
+                    return "무  기";
+                case "Helmet":
+                    return "모  자";
+                case "Shoes":
+                    return "신  발";
+                case "Ring":
+                    return "반  지";
+                case "Necklace":
+                    return "목걸이";
+                case "AddLuck":
+                    return "행  운";
+                case "AddDefence":
+                    return "방어력";
+                case "AddAttack":
+                    return "공격력";
+                case "AddIntelligence":
+                    return "지  력";
+                case "AddMaxHp":
+                    return "체  력";
+                case "AddMaxMp":
+                    return "마  나";
+                default:
+                    return "TranslateError";
+            }
+        }
     }
     
 
