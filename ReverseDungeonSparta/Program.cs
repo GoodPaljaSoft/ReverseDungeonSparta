ddb6df45506faff53cee76c7eba5ad53e76ad5f3
gyul-is-yummy <gyulisyummy@gmail.com>
2025-02-11 16:19:06 +0900

    [Fix] Win11 콘솔 버퍼 설정 안됨 이슈
    [Fix] Win11 콘솔 버퍼 설정 안됨 이슈
    
    콘솔창 크기 설정과 버퍼 크기 설정의 위치를 변경
    완벽한 해결은 아니지만 일단 터지진 않음

--- ReverseDungeonSparta/Program.cs
+++ ReverseDungeonSparta/Program.cs
@@ -5,9 +5,15 @@
         static void Main(string[] args)
         {
             //시작하기 전 콘솔 창 설정
+            Console.SetBufferSize(120, 300);            //버퍼 사이즈 지정 //넉넉하게 하지 않으면 터짐
             Console.SetWindowSize(120, 30);             //콘솔창 크기 지정
-            Console.SetBufferSize(120, 200);            //버퍼 사이즈 지정 //넉넉하게 하지 않으면 터짐
+            
             Console.Title = "REVERSE DUNGEON : SPARTA"; //콘솔창 제목 지정
+
+            //Console.WriteLine(Console.BufferWidth);
+            //Console.WriteLine(Console.BufferHeight);
+
+            //Console.ReadKey();
 
             ViewManager.width = Console.WindowWidth;
             ViewManager.height = Console.WindowHeight;
