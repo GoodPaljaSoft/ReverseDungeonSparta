cc76df9b39090ac8b61fba418355b872dc8abd9d
thdwpdn26 <songjeu26@naver.com>
2025-02-11 23:00:20 +0900

    [Feat]인벤토리 뷰 메니저 추가

---
+++ ReverseDungeonSparta/InventoryViewManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ReverseDungeonSparta.ViewManager;
+using static ReverseDungeonSparta.ViewManager3;
+
+namespace ReverseDungeonSparta
+{
+    public static class InventoryViewManager
+    {
+        public static void EnterInventoryMenuTxt()
+        {
+            Console.Clear();
+            PrintTitleTxt("소지 아이템", 12);
+            ViewManager.PrintText(0, 27, "   장비 아이템");
+            ViewManager.PrintText("소지 아이템");
+            ViewManager.PrintText("나가기");
+
+
+        }
+
+    }
+    
+
+}
